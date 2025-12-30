91215e3aecb8da9144bc98caf167b7991fc9a5d7
Cees Bos <dev@ceesbos.nl>
2025-12-30 15:12:25 +0100

    Remove URL from events and assignments as the link does not add more information.

--- scripts/functions.js
+++ scripts/functions.js
@@ -212,8 +212,8 @@ module.exports = async function () {
                 location: calevent.Lokatie,
                 description: desc,
                 method: 'PUBLISH',
-                status: Status,
-                url: 'https://'+mydomain+'/'
+                status: Status
+                // url: 'https://'+mydomain+'/'
             });
         });
 
@@ -243,8 +243,8 @@ module.exports = async function () {
                         allDay: true,
                         description: desc,
                         method: 'PUBLISH',
-                        status: 'CONFIRMED',
-                        url: 'https://'+mydomain+'/'
+                        status: 'CONFIRMED'
+                        // url: 'https://'+mydomain+'/'
                     });
             })
         }
